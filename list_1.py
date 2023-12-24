c686e5fa3e9350a7c771a57c4a168fb358d573ff
Zainul1573 <108649335+Zainul1573@users.noreply.github.com>
2023-12-24 17:53:12 +0530

    Update list_1.py

--- list_1.py
+++ list_1.py
@@ -3,9 +3,4 @@ list1 = [3, 5, 9, 11]
 list2 = [num**2 for num in list1 if num > 9]
 print(list2)
 
-fruits = ['apple', 'orange', 'banana']
 
-# Using a list comprehension to create a new list with each word repeated twice
-doubled_fruits = [fruit for fruit in fruits]
-
-print(doubled_fruits)
