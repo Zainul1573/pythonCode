c7ae0470d21edd2717ce45583ec3db73122b37a4
Zainul1573 <zainulameer@hotmail.com>
2023-12-26 22:33:50 +0530

    New Dict

--- Dict/dic1.py
+++ Dict/dic1.py
@@ -1,1 +1,33 @@
 # test
+
+
+dict_1 = {
+    'Student1': {
+        'Name': 'Nizar',
+        'Age': 20,
+        'Subject':{
+          'Maths': 'B',
+          'English': 'C'}
+        },
+
+    'Student2': {
+        'Name': 'Zainul',
+        'Age': 19,
+         'Subject': {
+             'Maths': 'C',
+             'English': 'A'}
+    }
+}
+print(dict_1)
+student_name = dict_1['Student1']['Name']
+student_age = dict_1['Student1']['Age']
+student_maths = dict_1['Student1']['Subject']['Maths']
+
+maths = dict_1['Student1']['Subject']['Maths']
+print(maths)
+
+print(f'Name :  {student_name}')
+print(f'Age :- {student_age}')
+print('----Exam Results:----')
+print(f'   Maths :- {student_maths}')
+print(f'   Maths :- {student_maths}')
