ff7faedcbe804a84c2835049103d57bc330d19e9
Zainul1573 <108649335+Zainul1573@users.noreply.github.com>
2023-12-26 19:32:55 +0530

    Update list_1.py

--- list/list_1.py
+++ list/list_1.py
@@ -1,1 +1,19 @@
 
+# list
+
+
+fruits = ['Apple', 'Mango', 'Orange']
+print(fruits)
+print()
+
+for fruit in fruits:
+    print(fruit)
+
+print()
+
+if 'Apple' in fruits:
+    print('Yes')
+    print()
+
+if 'Banana' not in fruits:
+    print('Banana is not this fruits list')
