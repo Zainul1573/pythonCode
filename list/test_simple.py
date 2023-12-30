7626c3ad86546c632f4be87f4cc295b94f4e6aed
Zainul1573 <zainulameer@hotmail.com>
2023-12-30 22:03:36 +0530

    New folder add

---
+++ list/test_simple.py
@@ -0,0 +1,7 @@
+# content of test_sample.py
+
+i = 1
+
+while i < 10:
+    print(i)
+    i += 1
---
+++ test.py
@@ -0,0 +1,12 @@
+
+class Student:
+
+    def year_11(self, name, reg_no):
+        self.name = name
+        self.reg_no = reg_no
+
+
+stu_year_11 = Student()
+
+stu_year_11.name = "Zainul"
+stu_year_11.reg_no = 111
